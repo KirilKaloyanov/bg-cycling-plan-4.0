7062c92188bb942ee66f7a90e4fa003dc9e3ff18
Kiril <kaloyanov.kiril@gmail.com>
2023-02-22 15:06:24 +0200

    Implemented sticky menu on scroll

--- menu.js
+++ menu.js
@@ -1,16 +1,28 @@
-fetch('menuTemplate.html')
-    .then(response => response.text())
-    .then(html => render(html));
+fetch("menuTemplate.html")
+  .then((response) => response.text())
+  .then((html) => render(html));
+
+let scrolling = false;
+window.onscroll = () => (scrolling = true);
 
 function render(html) {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-    const template = doc.querySelector('#menu-template');
-    const menuTarget = template.content.cloneNode(true);
-    document.querySelector('header').appendChild(menuTarget);
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+  const template = doc.querySelector("#menu-template");
+  const menuTarget = template.content.cloneNode(true);
+  document.querySelector("header").appendChild(menuTarget);
 
-    const headerDiv = document.querySelector('.header');
-    console.log(headerDiv.classList);
+  const headerDiv = document.querySelector(".header");
 
-    window.addEventListener('scroll', console.log(window.pageYOffset))
+  setInterval(() => {
+    if (scrolling) {
+      scrolling = false;
+      if (window.scrollY > 45) {
+        headerDiv.classList.add("ham");
+      } else {
+        headerDiv.classList.remove("ham");
+      }
+    }
+  }, 300);
 }
+
