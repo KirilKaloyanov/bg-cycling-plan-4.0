aa047ccc1bee8013d4a47048431ff92842bd023b
Kiril <kaloyanov.kiril@gmail.com>
2023-02-23 07:03:49 +0200

    Loading header component conditionally

--- header.js
+++ header.js
@@ -1,31 +1,35 @@
-fetch("templates/headerTemplate.html")
-  .then((response) => response.text())
-  .then((html) => renderHeader(html));
+function loadMenu() {
+  fetch("templates/headerTemplate.html")
+    .then((response) => response.text())
+    .then((html) => renderHeader(html));
 
-function stickyMenu(element) {
-  let scrolling = true;
-  window.onscroll = () => (scrolling = true);
+  function stickyMenu(element) {
+    let scrolling = true;
+    window.onscroll = () => (scrolling = true);
 
-  setInterval(() => {
-    if (scrolling) {
-      scrolling = false;
-      if (window.scrollY > 45) element.classList.add("ham");
-      else element.classList.remove("ham");
-    }
-  }, 100);
-}
+    setInterval(() => {
+      if (scrolling) {
+        scrolling = false;
+        if (window.scrollY > 45) element.classList.add("ham");
+        else element.classList.remove("ham");
+      }
+    }, 100);
+  }
 
-function renderHeader(html) {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, "text/html");
-  const headerTemplate = doc.querySelector("#menu-template");
-  const menuTarget = headerTemplate.content.cloneNode(true);
-  document.querySelector("header").appendChild(menuTarget);
+  function renderHeader(html) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    const headerTemplate = doc.querySelector("#menu-template");
+    const menuTarget = headerTemplate.content.cloneNode(true);
+    document.querySelector("header").appendChild(menuTarget);
 
-  const headerDiv = document.querySelector(".header");
-  stickyMenu(headerDiv);
+    const headerDiv = document.querySelector(".header");
+    stickyMenu(headerDiv);
 
-  const hamburger = document.querySelector('img.clickable');
-  const nav = document.querySelector('nav');
-  hamburger.addEventListener('click', () => nav.classList.toggle('container_hidden'));
+    const hamburger = document.querySelector("img.clickable");
+    const nav = document.querySelector("nav");
+    hamburger.addEventListener("click", () =>
+      nav.classList.toggle("container_hidden")
+    );
+  }
 }
