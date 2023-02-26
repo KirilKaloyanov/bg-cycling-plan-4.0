be6a4471bcdce9463cebf4cccbb6bb070f2b58a1
Kiril <kaloyanov.kiril@gmail.com>
2023-02-26 14:05:19 +0200

    Implemented footer component

--- footer.js
+++ footer.js
@@ -1,2 +1,14 @@
 const footer = document.querySelector('footer');
-console.log(footer);
+footer.classList.add('footer');
+
+const image = document.createElement('img');
+image.src = '/images/logoDTP.png';
+image.alt = 'Danube Transnational Programme logo';
+image.classList.add('dtpLogo');
+
+const footerText = document.createElement('div');
+footerText.classList.add('funding');
+footerText.textContent = 'Project is co-funded by the European Union funds (ERDF,IPA)';
+
+footer.appendChild(image);
+footer.appendChild(footerText);
