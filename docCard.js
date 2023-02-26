2ea7ba12683b26b35bea68ecf9658e1cb88e6005
Kiril <kaloyanov.kiril@gmail.com>
2023-02-26 15:15:03 +0200

    Document card custom component implemented

--- docCard.js
+++ docCard.js
@@ -1,14 +1,27 @@
 class DocumentCard extends HTMLElement {
-    // constructor() {
-    //     super();
-    //     this.attachShadow({ mode: 'open'});
-    // }
+
+    dtpUrl = "http://www.interreg-danube.eu/uploads/media/approved_project_" + this.getAttribute('link');
 
     connectedCallback() {
         const docCardTitle = document.createElement('div');
         docCardTitle.classList.add('document_card_title');
         docCardTitle.textContent = this.getAttribute('title');
-        this.appendChild(docCardTitle);
+
+        const cardConent = document.createElement('div');
+        cardConent.classList.add('card_content');
+        cardConent.appendChild(docCardTitle);
+
+        const docCard = document.createElement('div');
+        docCard.classList.add('document_card');
+        docCard.appendChild(cardConent);
+
+        const link = document.createElement('a');
+        link.classList.add('card_link');
+        link.href = this.dtpUrl;
+        link.target = '_blank';
+        link.appendChild(docCard);
+
+        this.appendChild(link);
     }
 }
 
